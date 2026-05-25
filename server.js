690e600d41a1cd0547bfcc21a9b70f2092549196
Cristian Straista <cristian@Cristians-MacBook-Pro-4.local>
2026-05-25 22:54:35 +0300

    Fix: PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { URL } = require('url');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const FEEDS = [
   // Moldova — surse verificate CJI
